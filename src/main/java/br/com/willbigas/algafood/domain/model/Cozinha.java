d11fb80720008294188019d1358ee633092e3c27
William Bigas Mauro <williambmauro@hotmail.com>
2020-05-04 17:10:36 -0300

    add biblioteca lombok para códigos boilerplate

--- src/main/java/br/com/willbigas/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/willbigas/algafood/domain/model/Cozinha.java
@@ -1,30 +1,23 @@
 package br.com.willbigas.algafood.domain.model;
 
-import javax.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table
 public class Cozinha {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- src/main/java/br/com/willbigas/algafood/domain/model/Restaurante.java
+++ src/main/java/br/com/willbigas/algafood/domain/model/Restaurante.java
@@ -1,6 +1,9 @@
 package br.com.willbigas.algafood.domain.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
