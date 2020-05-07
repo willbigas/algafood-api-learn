ef80b2df87f20b9eaea378c97029454d128183f5
William Bigas Mauro <williambmauro@hotmail.com>
2020-05-07 16:18:57 -0300

    Adiciondo PathVariable

--- src/main/java/br/com/willbigas/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/willbigas/algafood/api/controller/CozinhaController.java
@@ -5,13 +5,14 @@ import br.com.willbigas.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas" , produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 
     @Autowired
@@ -20,5 +21,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.buscar(id);
     }
 }
