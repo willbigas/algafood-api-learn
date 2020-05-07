61dbc0b540fd0aa242f0ee1445ce7132ffadaabe
William Bigas Mauro <williambmauro@hotmail.com>
2020-05-07 16:30:16 -0300

    Customizando XML com Annotations

--- src/main/java/br/com/willbigas/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/willbigas/algafood/domain/model/Cozinha.java
@@ -1,10 +1,12 @@
 package br.com.willbigas.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+//@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -15,6 +17,8 @@ public class Cozinha {
     @EqualsAndHashCode.Include
     private Long id;
 
+    //@JsonIgnore
+//    @JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
 
