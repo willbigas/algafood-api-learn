9487a551fb9d1a5810c972faf40ea66ae650b527
William Bigas Mauro <williambmauro@hotmail.com>
2021-10-13 16:51:57 -0300

    Aula 5.8

--- src/main/java/br/com/willbigas/algafood/api/controller/TesteController.java
+++ src/main/java/br/com/willbigas/algafood/api/controller/TesteController.java
@@ -34,6 +34,11 @@ public class TesteController {
         return cozinhaRepository.findByNome(nome);
     }
 
+    @GetMapping("/cozinhas/exists")
+    public Boolean cozinhaExists(String nome) {
+        return cozinhaRepository.existsByNome(nome);
+    }
+
     @GetMapping("/restaurantes/por-taxa-frete")
     public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial , BigDecimal taxaFinal) {
         return restauranteRepository.findByTaxaFreteBetween(taxaInicial , taxaFinal);
@@ -42,6 +47,21 @@ public class TesteController {
     @GetMapping("/restaurantes/por-nome")
     public List<Restaurante> restaurantesPorNome(String nome , Long cozinhaId) {
         return restauranteRepository.findByNomeContainingAndCozinhaId(nome , cozinhaId);
+    }
+
+    @GetMapping("/restaurantes/primeiro-por-nome")
+    public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
+        return restauranteRepository.findFirstByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/top2-por-nome")
+    public List<Restaurante> restauranteTop2PorNome(String nome) {
+        return restauranteRepository.findTop2ByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/count-por-cozinha")
+    public int restauranteCountPorCozinha(Long cozinhaId) {
+        return restauranteRepository.countByCozinhaId(cozinhaId);
     }
 
 
