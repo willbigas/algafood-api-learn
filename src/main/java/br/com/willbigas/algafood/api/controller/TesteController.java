b14c8508f47e281607f78b9ece6a9417aaae3cd3
William Bigas Mauro <williambmauro@hotmail.com>
2021-10-13 16:36:33 -0300

    Aula 5.8

--- src/main/java/br/com/willbigas/algafood/api/controller/TesteController.java
+++ src/main/java/br/com/willbigas/algafood/api/controller/TesteController.java
@@ -1,13 +1,16 @@
 package br.com.willbigas.algafood.api.controller;
 
 import br.com.willbigas.algafood.domain.model.Cozinha;
+import br.com.willbigas.algafood.domain.model.Restaurante;
 import br.com.willbigas.algafood.domain.repository.CozinhaRepository;
+import br.com.willbigas.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,9 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(String nome) {
         return cozinhaRepository.findAllByNomeContaining(nome);
@@ -27,4 +33,16 @@ public class TesteController {
     public Optional<Cozinha> cozinhaPorNome(String nome) {
         return cozinhaRepository.findByNome(nome);
     }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial , BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial , taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantesPorNome(String nome , Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome , cozinhaId);
+    }
+
+
 }
--- src/main/java/br/com/willbigas/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/willbigas/algafood/domain/repository/RestauranteRepository.java
@@ -4,9 +4,14 @@ import br.com.willbigas.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial , BigDecimal taxaFinal);
+
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 
 }
