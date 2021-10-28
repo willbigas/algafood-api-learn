3775a05004c3896054f036be7dc95674cf420cac
William Bigas Mauro <williambmauro@hotmail.com>
2021-10-28 17:16:06 -0300

    Aula 6.11

--- src/main/java/br/com/willbigas/algafood/api/controller/RestauranteController.java
+++ src/main/java/br/com/willbigas/algafood/api/controller/RestauranteController.java
@@ -29,6 +29,17 @@ public class RestauranteController {
 
     @GetMapping
     public List<Restaurante> listar() {
+        List<Restaurante> restaurantes = restauranteRepository.findAll();
+
+        System.out.println(restaurantes.get(0).getNome());
+        restaurantes.get(0).getFormasPagamento().forEach(System.out::println);
+
+        System.out.println(restaurantes.get(1).getNome());
+        restaurantes.get(1).getFormasPagamento().forEach(System.out::println);
+
+        System.out.println(restaurantes.get(0).getNome());
+        restaurantes.get(0).getFormasPagamento().forEach(System.out::println);
+
         return restauranteRepository.findAll();
     }
 
