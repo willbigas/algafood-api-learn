10ac2383896792a8137b9a49758bd0f3336b6099
William Bigas Mauro <williambmauro@hotmail.com>
2022-06-13 12:14:46 -0300

    11.14. Adicionando e usando o ModelMapper

--- src/main/java/br/com/willbigas/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/br/com/willbigas/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -1,25 +1,21 @@
 package br.com.willbigas.algafood.api.assembler;
 
 import br.com.willbigas.algafood.api.model.input.RestauranteInput;
-import br.com.willbigas.algafood.domain.model.Cozinha;
 import br.com.willbigas.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassembler {
 
+    private final ModelMapper modelMapper;
 
-    public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+    public RestauranteInputDisassembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
     }
 
+    public Restaurante toDomainObject(RestauranteInput restauranteInput) {
+        return modelMapper.map(restauranteInput, Restaurante.class);
+    }
 
 }
--- src/main/java/br/com/willbigas/algafood/api/assembler/RestauranteModelAssembler.java
+++ src/main/java/br/com/willbigas/algafood/api/assembler/RestauranteModelAssembler.java
@@ -1,8 +1,8 @@
 package br.com.willbigas.algafood.api.assembler;
 
-import br.com.willbigas.algafood.api.model.CozinhaModel;
 import br.com.willbigas.algafood.api.model.RestauranteModel;
 import br.com.willbigas.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,18 +11,14 @@ import java.util.stream.Collectors;
 @Component
 public class RestauranteModelAssembler {
 
+    private final ModelMapper modelMapper;
+
+    public RestauranteModelAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
     public RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteModel model = new RestauranteModel();
-        model.setId(restaurante.getId());
-        model.setNome(restaurante.getNome());
-        model.setTaxaFrete(restaurante.getTaxaFrete());
-        model.setCozinha(cozinhaModel);
-        return model;
+        return modelMapper.map(restaurante , RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
