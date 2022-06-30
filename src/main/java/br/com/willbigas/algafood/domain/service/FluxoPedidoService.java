90252ac0ff11db6392d0e2562193d755b7bfe4fa
William Bigas Mauro <williambmauro@hotmail.com>
2022-06-30 15:36:21 -0300

    12.23. Desafio: implementando endpoints de transição de status de pedidos

--- src/main/java/br/com/willbigas/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/br/com/willbigas/algafood/domain/service/FluxoPedidoService.java
@@ -33,4 +33,38 @@ public class FluxoPedidoService {
         pedido.setDataConfirmacao(OffsetDateTime.now());
 
     }
+
+    @Transactional
+    public void entregar(Long idPedido) {
+        Pedido pedido = pedidoService.buscarOuFalhar(idPedido);
+
+        if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
+            throw new NegocioException(
+                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
+                            pedido.getId(),
+                            pedido.getStatus().getDescricao(),
+                            StatusPedido.ENTREGUE.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.ENTREGUE);
+        pedido.setDataEntrega(OffsetDateTime.now());
+
+    }
+
+    @Transactional
+    public void cancelar(Long idPedido) {
+        Pedido pedido = pedidoService.buscarOuFalhar(idPedido);
+
+        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+            throw new NegocioException(
+                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
+                            pedido.getId(),
+                            pedido.getStatus().getDescricao(),
+                            StatusPedido.CANCELADO.getDescricao()));
+        }
+
+        pedido.setStatus(StatusPedido.CANCELADO);
+        pedido.setDataCancelamento(OffsetDateTime.now());
+
+    }
 }
