98f721c44dfdfc9340b1978f6232736b0957b1fe
William Bigas Mauro <williambmauro@hotmail.com>
2022-07-21 16:35:10 -0300

    Adicionado controle de cache no endpoint de listar cidades.

--- src/main/java/br/com/willbigas/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/willbigas/algafood/api/controller/CidadeController.java
@@ -5,11 +5,15 @@ import br.com.willbigas.algafood.domain.exception.NegocioException;
 import br.com.willbigas.algafood.domain.model.Cidade;
 import br.com.willbigas.algafood.domain.service.CidadeService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(value = "/cidades")
@@ -22,8 +26,11 @@ public class CidadeController {
     }
 
     @GetMapping
-    public List<Cidade> listar() {
-        return cidadeService.findAll();
+    public ResponseEntity<List<Cidade>> listar() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .body(cidadeService.findAll());
     }
 
     @GetMapping("/{id}")
@@ -37,7 +44,7 @@ public class CidadeController {
         try {
             return cidadeService.salvar(cidade);
         } catch (CidadeNaoEncontradaException e) {
-            throw new NegocioException(e.getMessage() , e);
+            throw new NegocioException(e.getMessage(), e);
         }
     }
 
@@ -48,7 +55,7 @@ public class CidadeController {
         try {
             return cidadeService.salvar(cidadeAtual);
         } catch (CidadeNaoEncontradaException e) {
-            throw new NegocioException(e.getMessage() , e);
+            throw new NegocioException(e.getMessage(), e);
         }
     }
 
