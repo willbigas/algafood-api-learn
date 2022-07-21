acc5e6a75e98d521f8965ddfea36da1ae69b9951
William Bigas Mauro <williambmauro@hotmail.com>
2022-07-21 15:15:52 -0300

    Adicionado configuracao de CORS para todo o projeto globalmente.

---
+++ src/main/java/br/com/willbigas/algafood/core/web/WebConfig.java
@@ -0,0 +1,21 @@
+package br.com.willbigas.algafood.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ *  Configura CORS para o projeto globalmente
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
