b9c0d67049520b85018521e0edf99c31c08da2b2
William Bigas Mauro <williambmauro@hotmail.com>
2022-07-23 21:53:35 -0300

    Adicionado default response codes spring docs

--- src/main/java/br/com/willbigas/algafood/core/openapi/OpenApiConfig.java
+++ src/main/java/br/com/willbigas/algafood/core/openapi/OpenApiConfig.java
@@ -5,6 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +28,28 @@ public class OpenApiConfig {
                 .externalDocs(new ExternalDocumentation()
                         .description("Código Fonte - GitHub")
                         .url("https://github.com/willbigas/algafood-api-learn"));
+    }
+
+    @Bean
+    public OpenApiCustomiser createDefaultResponseCodes() {
+
+        return openApi -> openApi
+                .getPaths()
+                .values()
+                .forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+                    ApiResponses apiResponses = operation.getResponses();
+                    ApiResponse apiResponse500 = getAPIResponse("Erro Interno do servidor");
+                    ApiResponse apiResponse406 = getAPIResponse("Recurso não possui representação que poderia ser aceita pelo servidor");
+
+                    apiResponses.addApiResponse("500", apiResponse500);
+                    apiResponses.addApiResponse("406", apiResponse406);
+                }));
+    }
+
+    private ApiResponse getAPIResponse(String description) {
+        return new ApiResponse()
+                .description(description)
+                .content(new Content()
+                        .addMediaType(org.springframework.http.MediaType.APPLICATION_JSON_VALUE, new MediaType()));
     }
 }
