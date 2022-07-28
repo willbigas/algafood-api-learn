73428cc1a62720018748b8ca74da3250ee7d3838
William Bigas Mauro <williambmauro@hotmail.com>
2022-07-28 11:30:17 -0300

    Adicionado Classe customizada de segurança do spring

---
+++ src/main/java/br/com/willbigas/algafood/core/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.willbigas.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/cozinhas/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+
+                .and()
+                .csrf()
+                .disable();
+
+
+
+        return http.build();
+    }
+}
